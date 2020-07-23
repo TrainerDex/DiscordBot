68ffc11140fdf36a3bcc32b42057c359c79a4242
Jay Turner <jay@trainerdex.co.uk>
2020-07-23 23:04:19 +0100

    Fix roles

--- tdx/core.py
+++ tdx/core.py
@@ -185,7 +185,7 @@ class TrainerDex(commands.Cog):
                     if team.id>0:
                         team_role = await getattr(self.config.guild(ctx.guild), ['mystic_role', 'valor_role', 'instinct_role'][team.id])()
                         if team_role:
-                            roles_to_add_on_join.append(team_role)
+                            roles_to_add_on_join.append(ctx.guild.get_role(team_role))
                 roles_to_remove_on_join=[ctx.guild.get_role(x) for x in roles_to_assign_on_approval['remove']]
                 member_edit_dict['roles'] = [x for x in (roles_to_add_on_join+mention.roles) if x not in roles_to_remove_on_join]
         
