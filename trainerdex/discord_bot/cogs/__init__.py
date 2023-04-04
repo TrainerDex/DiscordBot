3a10a6e6e8febae58140900e9d2c494c259b009d
Jay Turner <jaynicholasturner@gmail.com>
2023-04-04 19:24:55 +0100

    Simplify __init__ file

--- trainerdex/discord_bot/cogs/__init__.py
+++ trainerdex/discord_bot/cogs/__init__.py
@@ -1,6 +1,6 @@
-from trainerdex.discord_bot.cogs.interface import Cog  # noqa: F401
-from trainerdex.discord_bot.cogs.leaderboard import LeaderboardCog  # noqa: F401
-from trainerdex.discord_bot.cogs.mod import ModCog  # noqa: F401
-from trainerdex.discord_bot.cogs.post import PostCog  # noqa: F401
-from trainerdex.discord_bot.cogs.profile import ProfileCog  # noqa: F401
-from trainerdex.discord_bot.cogs.settings import SettingsCog  # noqa: F401
+from .interface import Cog  # noqa: F401
+from .leaderboard import LeaderboardCog  # noqa: F401
+from .mod import ModCog  # noqa: F401
+from .post import PostCog  # noqa: F401
+from .profile import ProfileCog  # noqa: F401
+from .settings import SettingsCog  # noqa: F401
