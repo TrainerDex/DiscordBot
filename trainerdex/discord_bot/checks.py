efe947e6caa4a983c8354dd989008e6fc143e2d2
Jay Turner <jaynicholasturner@gmail.com>
2023-04-04 21:26:48 +0100

    Remove more unused code

--- trainerdex/discord_bot/checks.py
+++ trainerdex/discord_bot/checks.py
@@ -1,37 +1,8 @@
-from typing import Callable, Optional
+from typing import Optional
 
-from discord import ApplicationContext, ChannelType, Member, Permissions
+from discord import ApplicationContext, Member
 
 from trainerdex.discord_bot.config import Config
-
-
-def has_permissions(permissions: Permissions) -> Callable[[ApplicationContext], bool]:
-    async def _has_permissions(ctx: ApplicationContext) -> bool:
-        if not (channel := ctx.interaction.channel) or channel.type != ChannelType.text:
-            return False
-
-        is_owner_ = await is_owner(ctx)
-
-        if not ctx.interaction.guild:
-            return False
-
-        is_guild_admin_ = await is_guild_admin(ctx)
-
-        has_permissions_ = channel.permissions_for(ctx.author) >= permissions
-
-        return is_owner_ or is_guild_admin_ or has_permissions_
-
-    return _has_permissions
-
-
-async def is_guild_admin(ctx: ApplicationContext) -> bool:
-    if isinstance(ctx.author, Member):
-        return ctx.author.guild_permissions.administrator or await is_owner(ctx)
-    return False
-
-
-async def is_owner(ctx: ApplicationContext) -> bool:
-    return await ctx.bot.is_owner(ctx.author)
 
 
 async def check_member_privilage(
