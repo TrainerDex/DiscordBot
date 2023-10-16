65703cfcbc5609a911c5c537c7598cd10839c207
Jay Turner <trainerdex@jayturnr.me>
2023-10-16 12:03:26 +0100

    Update mod.py

--- trainerdex/discord_bot/modules/mod.py
+++ trainerdex/discord_bot/modules/mod.py
@@ -54,8 +54,7 @@ class ModerationModule(Module):
         return bool(ctx.author.guild_permissions.manage_roles)
 
     def allowed_to_create_profiles(self) -> bool:
-        # return datetime.utcnow() < SHUTDOWN_DATE
-        return True
+        return datetime.utcnow() < SHUTDOWN_DATE
 
     def compare_stats(self, x: Update, y: Mapping[str, int | Decimal | None], /) -> bool:
         x_, y_ = vars(x), deepcopy(y)
